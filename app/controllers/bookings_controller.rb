97f2f53243c20be73e294bd7f24e81e43daa08e1
paulin tassi <paulintassi@hotmail.com>
2015-06-24 06:53:48 +0200

    finishing booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,39 @@
 class BookingsController < ApplicationController
+  before_action :set_reservation, only: [:show, :edit, :update, :destroy]
 
   def index
+    @bookings = Booking.all
   end
 
   def show
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.find(params[:id])
   end
 
   def new
-  end
-
-  def create
-  end
-
-  def edit
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new
+    # if @booking.save
+    #   @booking.update(user_id: current_user.id)
+    #   redirect_to root_path
+    # else
+    #   render :new
+    # end
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update_attributes(update_booking_params)
+    redirect_to :back
   end
 
+  private
+
+  def update_booking_params
+    params.require(:booking).permit(:accepted)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out)
+  end
 end
