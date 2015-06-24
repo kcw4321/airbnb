fe4f4913024c5cba26b1560709049c80ebdd4ab8
paulin tassi <paulintassi@hotmail.com>
2015-06-24 11:39:59 +0200

    improving booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,33 +1,34 @@
 class BookingsController < ApplicationController
-  before_action :set_reservation, only: [:show, :edit, :update, :destroy]
+  before_action :set_booking, only: [:show]
 
   def index
     @bookings = Booking.all
   end
 
   def show
-    @flat = Flat.find(params[:flat_id])
     @booking = Booking.find(params[:id])
   end
 
-  def new
-    @flat = Flat.find(params[:flat_id])
-    @booking = Booking.new
-    # if @booking.save
-    #   @booking.update(user_id: current_user.id)
-    #   redirect_to root_path
-    # else
-    #   render :new
-    # end
+  def create
+      @booking = Booking.new(flat_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def update
     @booking = Booking.find(params[:id])
-    @booking.update_attributes(update_booking_params)
+    @booking.update(update_booking_params)
     redirect_to :back
   end
 
   private
+
+  def set_booking
+    @flat = Flat.find(params[:flat_id])
+  end
 
   def update_booking_params
     params.require(:booking).permit(:accepted)
