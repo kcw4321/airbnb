6031bc3e6cf4b9567d80781a27f546621f0f8952
paulin tassi <paulintassi@hotmail.com>
2015-06-26 11:32:24 +0200

    nesting routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
       get 'profile'
     end
   end
-  resources :flats, except: :destroy
-  resources :bookings, except: :destroy
-
+  resources :flats, except: :destroy do
+    resources :bookings, except: :destroy
+  end
+  resources :bookings, only: :destroy
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
